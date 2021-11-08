b7721dc31772045ff42db96dbe6f0fa90522f094
Paulo Henrique <paulo.henrique@elastic.co>
2021-11-08 10:32:02 -0300

    adding dependencies / cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  ColorSchemeProvider,
+  GlobalStyles,
+  MantineProvider,
+} from "@mantine/styles";
+import { ColorSchemeSwitcher } from "components";
+import { useColorScheme } from "hooks/useColorScheme";
 
-function App() {
+export function App() {
+  const colorScheme = useColorScheme();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorSchemeProvider {...colorScheme}>
+      <MantineProvider
+        theme={{
+          // Override any other properties from default theme
+          fontFamily: "Open Sans, sans serif",
+          spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
+          colorScheme: colorScheme.colorScheme,
+        }}
+      >
+        <GlobalStyles />
+        <div>
+          <ColorSchemeSwitcher />
+          <header>
+            <h1>
+              Testing{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://mantine.dev/"
+              >
+                Mantine!
+              </a>
+            </h1>
+          </header>
+        </div>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
-
-export default App;
